3e67cf1643c2b38fb854282b0b0682afa5508251
gigi <gigichakaberia@gmail.com>
2026-09-02 20:49:45 +0400

    Set extra=0 for course inlines so empty forms are only added on button click

--- courses/admin.py
+++ courses/admin.py
@@ -5,20 +5,20 @@ from .models import Course, LearningObjective, Chapter, Lesson
 class LessonNestedInline(nested_admin.NestedTabularInline):
     model = Lesson
     fields = ('title', 'video_url', 'duration', 'order')
-    extra = 1
+    extra = 0
     sortable_field_name = "order"
 
 class ChapterNestedInline(nested_admin.NestedStackedInline):
     model = Chapter
     fields = ('title', 'order')
     inlines = [LessonNestedInline]
-    extra = 1
+    extra = 0
     sortable_field_name = "order"
 
 class LearningObjectiveNestedInline(nested_admin.NestedTabularInline):
     model = LearningObjective
     fields = ('text',)
-    extra = 1
+    extra = 0
 
 @admin.register(Course)
 class CourseAdmin(nested_admin.NestedModelAdmin):
